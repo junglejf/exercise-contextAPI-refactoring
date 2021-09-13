c760f2a0bbe4128a6e2267d972d47f72acb7f20c
junglejf <jfcchagas@id.uff.br>
2021-09-13 17:49:25 -0300

    exercise-one using provider and consumer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      }
+    };
+    this.moveCar = this.moveCar.bind(this);
+  }
+  moveCar(color) { 
+    this.setState((prevState)=>({ cars: { [color]: !prevState.cars[color]},}));
+  }
+  render () {
+    const { cars } = this.state;
+    const appCar = { cars , moveCar: this.moveCar };
+    return (
+        <MyContext.Provider value={appCar}>
+          <div>
+            <Cars />
+          </div>
+        </MyContext.Provider>
+      );
+  }
 }
 
 export default App;
